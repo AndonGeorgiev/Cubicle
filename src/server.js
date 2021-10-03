3fd618d3d05c09c7ed87983d0e0ea83f63ee38bc
AndonGeorgiev <andon2110@gmail.com>
2021-10-03 10:33:58 +0300

    Add express functionality

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('It is working');
+let express = require('express');
+
+let app = express();
+
+app.all('/', function(req, res) {
+    res.write('Hello, world!');
+    res.end();
+});
+
+app.listen(5000, function() {
+    console.log("App working on http://localhost:5000/");
+})
