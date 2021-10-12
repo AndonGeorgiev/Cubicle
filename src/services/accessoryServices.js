5a15337cacf82c30f0821464fc20c81dd4995615
AndonGeorgiev <andon2110@gmail.com>
2021-10-12 20:43:00 +0300

    add get only one accessory from DB fun.

--- src/services/accessoryServices.js
+++ src/services/accessoryServices.js
@@ -2,6 +2,8 @@ const Accessory = require('../model/Accessory');
 
 const getAll = () => Accessory.find({}).lean();
 
+const getOne = (id) => Accessory.findById(id).lean();
+
 function creat(name, description, imageUrl) {
     return Accessory.create({
         name,
@@ -14,6 +16,7 @@ function creat(name, description, imageUrl) {
 const accessoryService = {
     creat,
     getAll,
+    getOne,
 }
 
 module.exports = accessoryService;
