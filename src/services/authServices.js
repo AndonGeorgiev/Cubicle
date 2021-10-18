fd72fa338cf75be032d8de95d2ae2bc1d390a0a1
AndonGeorgiev <andon2110@gmail.com>
2021-10-18 23:07:57 +0300

    add create token function

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -8,8 +8,21 @@ function register(username, password, repeatPassword) {
     return User.create({ username, password });
 }
 
+function createToken(user) {
+    let payload = {
+        _id: user._id,
+        username: user.username,
+    };
+
+    let secret = '<<Hn%=:LuQ(*<?V"S<8x4_bOrK*62=B;SaouFn9issT@3)NY,M_5cZ}i1ysNqOq'
+
+    return authHelpers.jwtSign(payload, secret);
+
+}
+
 const authServices = {
     register,
+    createToken,
 }
 
 module.exports = authServices
